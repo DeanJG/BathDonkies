184f0254709e106220265b2999e4a8a16e3a58cf
ninaw1 <47039765+ninaw1@users.noreply.github.com>
2019-04-08 21:37:49 -0700

    Update finder.js

--- assets/js/finder.js
+++ assets/js/finder.js
@@ -1,45 +1,5 @@
-// Initialize Firebase
-const config = {
-    apiKey: "AIzaSyACZRFDd5HaZN_CloDFXSg1ILLZdWIrc8g",
-    authDomain: "testproject1-39f23.firebaseapp.com",
-    databaseURL: "https://testproject1-39f23.firebaseio.com",
-    projectId: "testproject1-39f23",
-    storageBucket: "testproject1-39f23.appspot.com",
-    messagingSenderId: "629671921712"
-};
-firebase.initializeApp(config);
-
-const db = firebase.firestore()
-// const auth = firebase.auth()
-
-
 /**  create authentication page
     when they move on from authenitcation page, set display : none
-    
-Move on to location page: set display of entering location/city  
-    Add in an enter button, with onclick even listener function to fetch the API information about places to eat 
-    Add in: user current location option function: 
-
-        navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position)
-        });
-
-Add in instructions page that explains swipe right/left/up/down actions
-
-Move on to main page where you view the randomized restaurants 
-    Use the math.floor function to randomize restaurants near the location entered 
-    Create two buttons for yes (on the right) or no (on the left)   
-        Add in swiping function from javascript library, swipe right for yes, left for no 
-    On the displayed card, dynamically add in information: 
-        1. Restaurant name
-        2. Restaurant image 
-        3. Restaurant description 
-        4. Restaurant distance 
-
-Kanye West is going to always be on this main page sitting on top of the restaurant card (almost like he is a mascot/static)
-    Always generating a random quote
-    Will always be there regardless of swiping left or right 
-
 
 If unique id is contained in rejectArray or favoriteArray, do not display/repeat this restaurant 
 When no is clicked: 
@@ -67,22 +27,64 @@ Ambitions:
     Cool features with the Kanye West API 
 **/
 
-// for initial location entry, note the necessity of location services
-// then, only use current lat/long if location field is empty
+$("#addToFavorites").on('click touchstart', function(){
+    $(this).toggleClass('is_animating')
+})
+  
+ /*when the animation is over, remove the class*/
+$("#addToFavorites").on('animationend', function(){
+     $(this).toggleClass('is_animating')
+    })
 
-// current location coords generator
-navigator.geolocation.getCurrentPosition((position) => {
-    const { latitude, longitude } = position.coords
-    console.log('latitude: ', latitude, 'longitude: ', longitude)
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyBXbVZnjOTHkSxkaB5ni4BM5T-xp8ghizQ",
+    authDomain: "firstproject-1a761.firebaseapp.com",
+    databaseURL: "https://firstproject-1a761.firebaseio.com",
+    projectId: "firstproject-1a761",
+    storageBucket: "firstproject-1a761.appspot.com",
+    messagingSenderId: "1003282022340"
+};
+firebase.initializeApp(config);
+
+let db = firebase.firestore()
+// const auth = firebase.auth()
 
 
-    // from location tag
+//Variable Declaration//
+let coordinates = {}
+    city = ``
 
-    // user's location
-    let lat1 = latitude,
-        lon1 = longitude
+
+//Function Declaration//
+
+// Function used for "x miles away" location tag
+const distFunc = (lat1, lon1, lat2, lon2) => {
+    let pi = Math.PI
+    const toRadians = (numInDegrees) => {
+        return numInDegrees * (pi / 180)
+    }
+    // barely modified formula from movable-type-scripts article
+    const R = 3958.8
+    let φ1 = toRadians(lat1)
+    let φ2 = toRadians(lat2)
+    let Δφ = toRadians((lat2 - lat1))
+    let Δλ = toRadians((lon2 - lon1))
+
+    let a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+
+    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    let d = R * c
+
+    return d
+}
+
+// Function for using coordinates to find nearby restaurants
+const fetchNearbyBusinesses = (lat1, lon1) => {
+    
     // to grab restaurant location from API
-    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=${lat1}&longitude=${lon1}`, {
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=${lat1}&longitude=${lon1}&limit=50&open_now=true`, {
         headers: {
             "Authorization": "Bearer CYpONbq3tuPRivns5oh_FenCfkuVArzigVu7ay4XjSaw1vOOZjWIgvQ7lPiyMRvXF2vlajmHLfHWqCUCBAKPVssu1NVAfDhuv9cHwFNwz8rgYI4W5FIg2DRY-CWcXHYx"
         }
@@ -90,58 +92,243 @@ navigator.geolocation.getCurrentPosition((position) => {
         .then(r => r.json())
         .then(r => {
             // random number generator for restaurant selection
+            let counter = 0
+            const getRest = _ => {
             let randRest = Math.floor(Math.random() * r.businesses.length)
-            console.log(randRest)
-            // pulling coordinates for destination
-            let { latitude: lat2, longitude: lon2 } = r.businesses[randRest].coordinates
-
-            // Formula used for "x miles away" location tag
-            // needed to manufacture a way to convert to radians
-            let pi = Math.PI
-            const toRadians = (numInDegrees) => {
-                return numInDegrees * (pi / 180)
+            let ratingThreshold = 2.5
+            let randBusiness = r.businesses[randRest]
+            counter++
+            if (randBusiness.rating >= ratingThreshold) {
+                return randBusiness
+            }else {
+                if (counter > r.businesses.length) {
+                return null
+                }
+                getRest()
             }
-            // barely modified formula from movable-type-scripts article
-            let R = 3958.8
-            let φ1 = toRadians(lat1)
-            let φ2 = toRadians(lat2)
-            let Δφ = toRadians((lat2 - lat1))
-            let Δλ = toRadians((lon2 - lon1))
+        }
+        const highRatedRest = getRest()
+            // filter to display only 2.5+ rated restaraunts
+            if (highRatedRest) {
+                // pulling coordinates for destination
+                let { latitude: lat2, longitude: lon2 } = highRatedRest.coordinates
 
-            let a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+                // Function used for "x miles away" location tag
+                const distance = distFunc(lat1, lon1, lat2, lon2)
 
-            let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+                if (distance < 1) {
+                    // shows distance in decimals if between 0 and 1
+                    console.log(`${highRatedRest.name} is ~ ${distance.toFixed(2)} Miles Away`)
+                } else {
+                    // shows rounded distance otherwise
+                    console.log(`${highRatedRest.name} is ~ ${Math.round(distance)} Miles Away`)
+                }
+                document.getElementById('restImage').src = `${highRatedRest.image_url}`
+                document.querySelector('.card-title').textContent = `${highRatedRest.name}`
+                document.querySelector('.card-price').textContent = `${highRatedRest.price}`
+                document.querySelector('.card-transaction').textContent = `${highRatedRest.transactions}`
+                document.querySelector('.card-telephone').textContent = `${highRatedRest.display_phone}`
+                document.querySelector('.card-url').textContent = `${highRatedRest.url}`
 
-            let d = R * c
-
-            console.log(`${r.businesses[randRest].name} is ~ ${Math.round(d)} Miles Away`)
-
-            console.log(r)
-            // .map returns a new array for us 
-            // .join(' ') joins array and separates with a space
-            const categories = r.businesses[randRest].categories
-                .map(category => category.title)
-                .join(' ')
-
-            document.querySelector('.card-type').textContent = `Type of food: ${categories}`
 
 
-            document.getElementById('restImage').src =`${r.businesses[randRest].image_url}`
+                // .map returns a new array for us 
+                // .join(' ') joins array and separates with a space
+                const categories = highRatedRest.categories
+                    .map(category => category.title)
+                    .join(' ')
 
-            document.querySelector('.card-title').textContent=`${r.businesses[randRest].name}`
+                document.querySelector('.card-type').textContent = `Type of food: ${categories}`
 
-            document.querySelector('.card-price').textContent=`${r.businesses[randRest].price}`
-
-            document.querySelector('.card-transaction').textContent=`${r.businesses[randRest].transactions}`
-
-            // the url button to work
-            const url = r.businesses[randRest].url
-            window.location = url
-
+                document.getElementById("buttonUrl").onclick = function () {
+                    location.href = (highRatedRest.url)
+                }
+            }
         })
         .catch(e => console.error(e))
+}
+
+// function for searching nearby restaurants based on city text input
+const fetchNearbyBusinessesCity = (city, lat1, lon1) => {
+    // to grab restaurant location from API
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${city}&limit=50&open_now=true`, {
+        headers: {
+          Authorization:
+            "Bearer CYpONbq3tuPRivns5oh_FenCfkuVArzigVu7ay4XjSaw1vOOZjWIgvQ7lPiyMRvXF2vlajmHLfHWqCUCBAKPVssu1NVAfDhuv9cHwFNwz8rgYI4W5FIg2DRY-CWcXHYx"
+        }
+      }
+    )
+    .then(r => r.json())
+    .then(r => {
+        // random number generator for restaurant selection
+        let counter = 0
+        const getRest = _ => {
+            let randRest = Math.floor(Math.random() * r.businesses.length)
+            let ratingThreshold = 2.5
+            let randBusiness = r.businesses[randRest]
+            counter++
+            if (randBusiness.rating >= ratingThreshold) {
+                return randBusiness
+            }else {
+                if (counter > r.businesses.length) {
+                return null
+                }
+                getRest()
+            }
+        }
+        const highRatedRest = getRest()
+        // filter to display only 2.5+ rated restaraunts
+        if (highRatedRest) {
+            // pulling coordinates for destination
+            let { latitude: lat2, longitude: lon2 } = highRatedRest.coordinates
+
+            // Function used for "x miles away" location tag
+            const distance = distFunc(lat1, lon1, lat2, lon2)
+
+            if (distance < 1) {
+                // shows distance in decimals if between 0 and 1
+                console.log(`${highRatedRest.name} is ~ ${distance.toFixed(2)} Miles Away`)
+            } else {
+                // shows rounded distance otherwise
+                console.log(`${highRatedRest.name} is ~ ${Math.round(distance)} Miles Away`)
+            }
+            document.getElementById("restImage").src = `${highRatedRest.image_url}`
+            document.querySelector(".card-title").textContent = `${highRatedRest.name}`
+            document.querySelector(".card-price").textContent = `${highRatedRest.price}`
+            document.querySelector(".card-transaction").textContent = `${highRatedRest.transactions}`
+            document.querySelector('.card-telephone').textContent = `${highRatedRest.display_phone}`
+            document.querySelector('.card-url').textContent = `${highRatedRest.url}`
+
+            // .map returns a new array for us
+            // .join(' ') joins array and separates with a space
+            const categories = highRatedRest.categories.map(category => category.title).join(" ")
+
+            document.querySelector(".card-type").textContent = `Type of food: ${categories}`
+
+            document.getElementById("buttonUrl").onclick = _ => {location.href = highRatedRest.url}
+        }
+    })
+    .catch(e => console.error(e))
+}
+
+//Page Functionality//
+
+// current location coords generator
+navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords
+    console.log('latitude: ', latitude, 'longitude: ', longitude)
+
+    // user's location
+    coordinates = {
+        lat1: latitude,
+        lon1: longitude
+    }
+    return coordinates
 })
 
+
+
+// prevents enter key default
+document.querySelector(`#locationInput`).addEventListener(`keydown`, e => {
+    if (e.keyCode === 13) {
+        e.preventDefault()
+    }
+})
+
+// differentiates which fetch request to run
+document.querySelector('.search').addEventListener(`click`, e => {
+    // // variable to house city input
+    city = document.getElementById(`locationInput`).value
+
+    if (city === ``) {
+        console.log(`search coords`)
+        console.log(coordinates)
+        fetchNearbyBusinesses(coordinates.lat1, coordinates.lon1)
+    }else {
+        console.log(`search a city`)
+        console.log(`city`)
+        fetchNearbyBusinessesCity(city, coordinates.lat1, coordinates.lon1)
+    }
+})
+
+// Firebase integration and favorites page visualization //
+
+//adding to firebase favorite
+document.getElementById('addToFavorites').addEventListener('click', e => {
+    e.preventDefault()
+    document.querySelector("#heartImage").style.display = "block"
+    
+    db.collection("Favorites").doc(document.querySelector('.card-title').textContent).set({
+        name: document.querySelector('.card-title').textContent,
+        typeoffood: document.querySelector('.card-type').textContent,
+        dollars: document.querySelector('.card-price').textContent,
+        transaction: document.querySelector('.card-transaction').textContent,
+        telephone: document.querySelector('.card-telephone').textContent,
+        url: document.querySelector('.card-url').textContent,
+    })
+    .then(function() {
+        setTimeout(() => {
+            document.querySelector("#heartImage").style.display = "none"
+        }, 1000)
+        console.log("Document successfully written!");
+    })
+    // reruns city/coords search functions on "Favorites" press
+    city = document.getElementById(`locationInput`).value
+
+    if (city === ``) {
+        console.log(`search coords`)
+        console.log(coordinates)
+        fetchNearbyBusinesses(coordinates.lat1, coordinates.lon1)
+    }else {
+        console.log(`search a city`)
+        console.log(`city`)
+        fetchNearbyBusinessesCity(city, coordinates.lat1, coordinates.lon1)
+    }
+})
+
+//adding to firebase rejected
+document.getElementById('notFavorite').addEventListener('click', e => {
+    e.preventDefault()
+
+    db.collection("Dislikes").doc(document.querySelector('.card-title').textContent).set({
+        name: document.querySelector('.card-title').textContent,
+        typeoffood: document.querySelector('.card-type').textContent,
+        dollars: document.querySelector('.card-price').textContent,
+        transaction: document.querySelector('.card-transaction').textContent,
+        telephone: document.querySelector('.card-telephone').textContent,
+    })
+    .then(function() {
+        console.log("Document successfully written!");
+    })
+    // reruns city/coords search functions on "Dislike" press
+    city = document.getElementById(`locationInput`).value
+
+    if (city === ``) {
+        console.log(`search coords`)
+        console.log(coordinates)
+        fetchNearbyBusinesses(coordinates.lat1, coordinates.lon1)
+    }else {
+        console.log(`search a city`)
+        console.log(`city`)
+        fetchNearbyBusinessesCity(city, coordinates.lat1, coordinates.lon1)
+    }
+})
+    
+//showing favorites(DOM)
+db.collection('Favorites').onSnapshot(({ docs }) => {
+    document.querySelector('.favorites').innerHTML = ''
+    docs.forEach(doc => {
+        let { name, dollars, transaction, typeoffood} = doc.data()
+        let docElem = document.createElement('div')
+        docElem.innerHTML = `
+            <h3>${name}</h3>
+            <h4>${dollars}</h4>
+            <h6>${transaction}</h6>
+            <h6>${typeoffood}</h6>
+            `
+        document.querySelector('.favorites').append(docElem)
+    })
+})
 
 // kanye west API key fetch
 fetch(`https://api.kanye.rest`) 
@@ -149,8 +336,43 @@ fetch(`https://api.kanye.rest`)
 .then(r => {
     // console log r to show random quote from kanye west
     // console.log(r)
-    document.querySelector('#section').textContent = `Kanye's food for thought : ${r.quote}` 
+
+    document.querySelector('#section').textContent = `Kanye's Food For Thought : ${r.quote}` 
 })
 
-// fetching Yelp API
+//////******************** Screen Toggle *//////
 
+// login to location page
+function dispFunction() {
+    //display
+    document.getElementById("locationPage").style.display = "block";
+    //hide
+    document.getElementById("loginPage").style.display = "none";
+    
+}
+
+// display main page
+function dispMainPage() {
+    //display
+    document.getElementById("mainPage").style.display = "block";
+    // hide
+    document.getElementById("locationPage").style.display = "none";
+}
+// to favorites page
+function dispFaves() {
+    document.getElementById("favoritesPage").style.display = "block";
+    document.getElementById("mainPage").style.display = "none"; 
+    document.getElementById("locationPage").style.display = "none";
+
+}
+// back to location page
+function dispLocationPage() {
+    document.getElementById("locationPage").style.display = "block";
+    document.getElementById("mainPage").style.display = "none";
+    document.getElementById("favoritesPage").style.display = "none";
+
+}
+
+// heart animation
+
+/* when a user clicks, toggle the 'is-animating' class */
